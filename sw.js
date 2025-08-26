427d5e3fc4f7ce53aa0fab6faf26a11f1b25ad54
dustinbroussard <dustin.broussard@gmail.com>
2025-08-26 11:28:51 -0500

    Add sticky footer controls and fix modal stacking

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hill-rd-setlist-manager-v1';
+const CACHE_NAME = 'hill-rd-setlist-manager-v2';
 const urlsToCache = [
     '/',
     '/index.html',
@@ -11,7 +11,7 @@ const urlsToCache = [
     '/assets/icons/icon-192x192.png',
     '/assets/icons/icon-512x512.png',
     '/assets/images/mylogo.png',
-    'lib/mammoth.browser.min.js',
+    '/lib/mammoth.browser.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
     'https://cdn.jsdelivr.net/npm/fuse.js@6.6.2/dist/fuse.min.js'
 ];
